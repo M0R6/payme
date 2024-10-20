c95e293c0b995938278faa0dd1592fe2de42fcc6
MORGAN <tellmesomething.morgan@gmail.com>
2024-10-20 09:02:13 +0700

    Pay To Morgan v1

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app class="h-screen pa-5 background-photo align-center">
+    <div class="mx-auto my-auto justify-center align-center">
+      <img class="responsive-img" src="/assets/MorganQRIS.svg">
+      <h1 class="text-center barriecito-regular text-4xl mt-5">
+        Morgan's QRIS
+      </h1>
+    </div>
+   </v-app>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Barriecito&display=swap');
+
+.barriecito-regular {
+  font-family: "Barriecito", system-ui;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
+
+<style scoped>
+
+.responsive-img {
+  max-width: 100%;
+  height: auto;
+}
+
+.background-photo {
+  background-image: url('/assets/unsplashBG.jpg') !important;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
+
